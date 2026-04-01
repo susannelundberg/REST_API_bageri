dda068272baf828087d773f1b771aaaaa11b457f
susannelundberg <lundbergsusanne@outlook.com>
2026-04-01 20:38:30 +0200

    Added SupplierProductsController

--- Controllers/SupplierProductsController.cs
+++ Controllers/SupplierProductsController.cs
@@ -4,16 +4,23 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+using Microsoft.EntityFrameworkCore;
 
 namespace BageriApi.Controllers;
 
-[Route("api/products")]
+[Route("api/supplierproducts")]
 [ApiController]
 public class SupplierProductsController(BageriContext context) : ControllerBase
 {
     [HttpPost()]
     public async Task<ActionResult> AddSupplierProduct(SupplierProduct supplierProduct)
     {
+        Supplier supplier = await context.Suppliers.FindAsync(supplierProduct.SupplierId);
+        if(supplier is null) return BadRequest("Leverantör existerar inte");
+
+        Product product = await context.Products.FindAsync(supplierProduct.ProductId);
+        if(product is null) return BadRequest("Produkten existerar inte");
+        
         SupplierProduct item = new()
         {
             ProductId = supplierProduct.ProductId,
@@ -25,5 +32,38 @@ public class SupplierProductsController(BageriContext context) : ControllerBase
         await context.SaveChangesAsync();
         return Ok();
 
+    }
+
+    [HttpGet()]
+    public async Task<ActionResult> ListAllSupplierProducts()
+    {
+        var item = await context.SupplierProducts
+        .Include(c => c.Supplier)
+        .Select(supplierProduct => new
+        {
+            ProductName = supplierProduct.Product.Name,
+            SupplierName = supplierProduct.Supplier.Name
+        }).ToListAsync();
+
+        return Ok(new {Success = true, StatusCode = 200, Items = item.Count, Data = item});
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> FindProduct(int id)
+    {
+        var supplierProduct = await context.SupplierProducts
+        .Where(sp => sp.ProductId == id)
+        .Include(sp => sp.Product)
+        .Include(sp => sp.Supplier)
+        .ToListAsync();
+
+        var data = supplierProduct.Select(sp => new
+        {
+            ProductName = sp.Product.Name,
+            SupplierName = sp.Supplier.Name,
+            Price = sp.PricePerKg
+        });
+
+        return Ok(new { Success = true, StatusCode = 200, Items = "Not defined", Data = data });
     }
 }
