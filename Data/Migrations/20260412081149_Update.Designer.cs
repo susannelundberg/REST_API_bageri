887460eee27cd5f58148633b1435870f055281f5
susannelundberg <lundbergsusanne@outlook.com>
2026-04-12 10:12:21 +0200

    Update migrations and database

--- Data/Migrations/20260412081149_Update.Designer.cs
+++ Data/Migrations/20260412081149_Update.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BageriApi.Data.Migrations
 {
     [DbContext(typeof(BageriContext))]
-    [Migration("20260412063222_Update database")]
-    partial class Updatedatabase
+    [Migration("20260412081149_Update")]
+    partial class Update
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- Data/Migrations/20260412081149_Update.cs
+++ Data/Migrations/20260412081149_Update.cs
@@ -5,7 +5,7 @@
 namespace BageriApi.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class Updatedatabase : Migration
+    public partial class Update : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
